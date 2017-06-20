d1f2dbd02535e18695a90f794d8396ceceb296ae
esf_tanwb <xz_it_bent@esuperfund.com.au>
2017-06-20 10:37:45 +0800

    ad

--- XBChat/routes/index.js
+++ XBChat/routes/index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
-
+var request = require('request');
+var url = 'api';
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {
-      title: 'Chat'
-    });
+  res.render('index', {
+    title: 'Chat'
+  });
 });
+
+router.post('/send', function (req, res, next) {
+  console.log(req.body);
+
+  res.json({
+    code: '1',
+    msg: 'ok'
+  });
+});
+
 module.exports = router;
