f19da300b5e27cef19a15dea831bc610506b7c8f
twb0223 <twb0223@gmail.com>
2017-11-03 17:28:50 +0800

    update package

--- VueChat/build/dev-server.js
+++ VueChat/build/dev-server.js
@@ -30,12 +30,12 @@ const http = require('http');
 
 const https = require('https');
 
-var bodyParser = require('body-parser');
-// var multer = require('multer');
+// var bodyParser = require('body-parser');
+// // var multer = require('multer');
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-// app.use(multer()); // for parsing multipart/form-data
+// app.use(bodyParser.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// // app.use(multer()); // for parsing multipart/form-data
 
 io.on('connection', (socket) => {
   // 群聊
