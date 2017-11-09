c2396a66d50f87ad4cd741ed8d3e93a4c599611a
twb0223 <twb0223@gmail.com>
2017-11-09 14:43:16 +0800

    add vue

---
+++ VueChat/src/components/Emoji.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="emoji">
+      <ul>
+          <li></li>
+      </ul>
+    </div>    
+</template>
+
+<script>
+export default {
+    data(){
+        return {};
+    },
+    method:{
+        showall(){
+            //todo 构造👿显示列表
+        },
+        select(){}
+    },
+};
+</script>
+
+<style>
+
+</style>
